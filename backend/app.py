26e7674193ea6bc7bdac59174769ef9027ff4fc3
Rudra Barot <rudrabarot05@gmail.com>
2026-05-11 22:07:42 -0400

    Prepare backend for deployment

--- backend/app.py
+++ backend/app.py
@@ -35,4 +35,6 @@ def home():
     return {'message': 'Ripple backend is running!'}
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+port = int(os.environ.get('PORT', 5000))
+app.run(debug=False, host='0.0.0.0', port=port)
